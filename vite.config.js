87ab8198a178870b8bb3c0b3bc56293ad683ba46
kabilanero <kapilrhode0000@gmail.com>
2025-03-19 23:55:26 +0530

    2nd clean 2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Attendance_compiler/',   // Base path for GitHub Pages
+  base: '/New_attendance/',   // Base path for GitHub Pages
   plugins: [react()],
   build: {
     outDir: 'dist',
